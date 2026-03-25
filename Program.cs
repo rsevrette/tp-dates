c79c39fc8ee53039d33392b093e6eef6b338dbcf
rsevrette <romain.sevrette@edouardgand.fr>
2026-03-25 15:48:55 +0100

    Partie 4

--- Program.cs
+++ Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Partie 1
+        Console.WriteLine("\n_______________Partie 1_______________");
+        DateTime dateActuelle = DateTime.Now;
+
+        Console.WriteLine(dateActuelle);
+        Console.WriteLine(dateActuelle.ToString("dd/MM/yyyy"));
+
+        //Partie 2
+        Console.WriteLine("\n_______________Partie 2_______________");
+        DateTime date1janvier = new DateTime(2025, 1, 1);
+        Console.WriteLine(date1janvier);
+        Console.WriteLine(date1janvier.ToString("dd/MM/yyyy"));
+
+        Console.WriteLine("\n_______________Partie 3_______________");
+        Console.WriteLine(date1janvier.ToShortDateString());
+        Console.WriteLine(date1janvier.ToLongDateString());
+        Console.WriteLine(date1janvier.ToString("dd/MM/yyyy"));
+        Console.WriteLine(date1janvier.ToString("yyyy-MM-dd"));
+
+        Console.WriteLine("\n_______________Partie 4_______________");
+        
     }
 }
