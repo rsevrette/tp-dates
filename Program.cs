c9465709828c97927a809014ce875cef5e2961d2
rsevrette <romain.sevrette@edouardgand.fr>
2026-03-25 16:01:34 +0100

    Partie 5

--- Program.cs
+++ Program.cs
@@ -24,6 +24,21 @@ class Program
         Console.WriteLine(date1janvier.ToString("yyyy-MM-dd"));
 
         Console.WriteLine("\n_______________Partie 4_______________");
-        
+        DateTime date1 = date1janvier.AddDays(10);
+        Console.WriteLine(date1.ToLongDateString());
+
+        DateTime date2 = date1janvier.AddMonths(2);
+        Console.WriteLine(date2.ToLongDateString());
+
+        DateTime date3 = date1janvier.AddYears(1);
+        Console.WriteLine(date3.ToLongDateString());
+
+        Console.WriteLine("\n_______________Partie 5_______________");
+        DateTime date4 = date1janvier.AddDays(-5);
+        Console.WriteLine(date4.ToLongDateString());
+
+        DateTime date5 = date1janvier.AddMonths(-1);
+        Console.WriteLine(date5.ToLongDateString());
+
     }
 }
